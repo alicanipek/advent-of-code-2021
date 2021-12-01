e868ac597a68c31d5c91b3fa54d22e626983349c
Ali Can İPEK <alican.ipek@outlook.com>
2021-12-01 22:40:30 +0300

    day1 solutions

---
+++ Solutions/Day1/SonarSweep.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace AdventOfCode2021.Solutions.Day1 {
+	public class SonarSweep {
+		private readonly List<int> Depths = new List<int>();
+
+		public SonarSweep()
+		{
+			var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input/Day1/input.txt");
+			foreach (var str in File.ReadAllLines(path))
+			{
+				Depths.Add(int.Parse(str));
+			}
+		}
+		public int CalculateIncreaseCount()
+		{
+			var count = 0;
+			for (int i = 0; i < Depths.Count - 1; i++)
+			{
+				if (Depths[i + 1] > Depths[i])
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+		public int CalculateWindowIncreaseCount()
+        {
+			var count = 0;
+			for (int i = 0; i < Depths.Count - 3; i++)
+			{
+				var firstWindow = Depths[i] + Depths[i + 1] + Depths[i + 2];
+				var secondWindow = Depths[i+1] + Depths[i + 2] + Depths[i + 3];
+				if (secondWindow > firstWindow)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	}
+}
